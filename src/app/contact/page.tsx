37dc97485c7004560d24b78fdb59719da148a2c2
Zachary <zjerome@umich.edu>
2026-01-07 10:42:03 -0500

    header logo

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -12,10 +12,10 @@ export const metadata: Metadata = {
 const ContactPage = () => {
   return (
     <>
-      <Breadcrumb
+      {/* <Breadcrumb
         pageName="Contact Page"
         description="Lorem ipsum dolor sit amet, consectetur adipiscing elit. In varius eros eget sapien consectetur ultrices. Ut quis dapibus libero."
-      />
+      /> */}
 
       <Contact />
     </>
