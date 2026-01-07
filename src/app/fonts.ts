e078cb2a9311859087c53c1edfcad95a7eca2553
Zachary <zjerome@umich.edu>
2026-01-07 13:32:37 -0500

    contact us

--- src/app/fonts.ts
+++ src/app/fonts.ts
@@ -1,7 +1,7 @@
 import localFont from "next/font/local";
 
 export const aerospace = localFont({
-  src: "../public/fonts/Aerospace.ttf",
+  src: "../../public/fonts/Aerospace.ttf",
   variable: "--font-aerospace",
   display: "swap",
 });
